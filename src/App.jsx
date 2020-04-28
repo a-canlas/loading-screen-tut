486d3c558f2e91b68820a8941262c7132a426237
Anastasia Canlas <recr38@gmail.com>
2020-04-28 15:29:08 -0700

    Defined fetch method getGitHubInfo in App
    Set up a fetch call to api/users of GitHub
    Added getGitHubInfo method call in componentDidMount
    Commented out wait method call in componentDidMount
    Added comments describing wait and sleep methods

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,47 @@ class App extends React.Component {
     };
   }
 
+  /* Non-fetch methods */
+
+  // Sets an async Timeout by instantiating a new Promise object
   sleep = milliseconds => {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
   };
 
-  wait = async (milliseconds = 2000) => {
+  // The await keyword causes the code to pause at that line leaving the other code in application to execute
+  wait = async milliseconds => {
     await this.sleep(milliseconds);
     this.setState({
-      name: "Anastasia",
-      login: "matcha",
+      name: "Redwulf",
+      login: "beach-bum-wulf",
       location: "California",
       isLoading: false
     });
   };
 
+  /* Fetch method using GitHub API */
+
+  getGitHubInfo() {
+    fetch("https://api.github.com/users/a-canlas")
+      .then(data => data.json())
+      .then(data => {
+        this.setState({
+          name: data.name,
+          location: data.location,
+          login: data.login,
+          isLoading: false
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   componentDidMount() {
-    this.wait();
+    this.wait(2000);
+    // this.getGitHubInfo();
   }
 
   render() {
-    let name = "Anastasia";
-    let location = "California";
-    let login = "matcha";
+    let { name, login, location } = this.state;
     const details = { name, location, login };
     return this.state.isLoading ? <Loader /> : <ShowDetail details={details} />;
   }
